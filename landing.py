ac5c4d0315609f886aaa984b0a11b64e1995a505
alinooralian <144942445+alinooralian@users.noreply.github.com>
2026-01-09 21:07:56 +0330

    New Load Game

--- landing.py
+++ landing.py
@@ -1,6 +1,9 @@
 import re
-import json
 import uuid
+import json
+import time
+import os
+from pathlib import Path
 
 with open("users.json", "r") as f:
     try:
@@ -8,11 +11,7 @@ with open("users.json", "r") as f:
     except json.JSONDecodeError:
         users = {}
 
-with open("players.json", "r") as f:
-    try:
-        players = json.load(f)
-    except json.JSONDecodeError:
-        players = {}
+players = dict()
 
 def signup():
     username = input("Enter your username: ")
@@ -38,20 +37,14 @@ def signup():
     players[userid] = {
         "username" : username,
         "password" : password,
-        "cash" : 120,
-        "position" : 0,
-        "property" : [],
-        "jail" : False,
-        "get_out_of_jail_card" : False,
-        "dice_counter" : 3,
-        "bankrupt" : False
+        "money" : 120,
+        "property" : 0,
+        "prsion" : False
     }
     with open("users.json", "w", encoding="utf-8") as f:
         json.dump(users, f, ensure_ascii=False, indent=4)
-    with open("players.json", "w", encoding="utf-8") as f:
+    with open(path, "w", encoding="utf-8") as f:
         json.dump(players, f, ensure_ascii=False, indent=4)
-        
-    print(f"Hello {username}! You are ready to play.")
     return True
 def login(Type):
     username = input("Enter your username: ")
@@ -70,7 +63,7 @@ def login(Type):
                         "property" : 0,
                         "prsion" : False
                     }
-                    with open("players.json", "w", encoding="utf-8") as f:
+                    with open(path, "w", encoding="utf-8") as f:
                         json.dump(players, f, ensure_ascii=False, indent=4)
                     return True
                 break
@@ -85,8 +78,8 @@ def login(Type):
     else:
         print("Username not found.")
     
-    print(f"Hello {username}! You are ready to play.")
     return False
+
 
 print("1.New Game")
 print("2.Load Game")
@@ -95,16 +88,21 @@ print("4.Exit")
 key = input()
 
 if key == '1':
-    with open("players.json", "w", encoding="utf-8") as f:
-        json.dump({}, f)
+    game_name = input("Enter your game name: ") + ".json"
+    path = Path(__file__).parent / "old_games" / game_name
     
-    with open("players.json", "r") as f:
+    with open(path, "x", encoding="utf-8") as f:
+        pass
+    
+    with open(path, "r", encoding="utf-8") as f:
         try:
             players = json.load(f)
         except json.JSONDecodeError:
             players = {}
     
-    while len(players) <= 4:
+    while True:
+        if len(players) >= 4:
+            break
         print("1.Singup")
         print("2.Login")
         print("3.Exit")
@@ -114,14 +112,35 @@ if key == '1':
             while True:
                 if signup():
                     break
-        elif key == '2':
+        
+        if key == '2':
             while True:
                 if login(1):
                     break
         
         if key == '3':
             exit()
+
 elif key == '2':
+    game_name = input("Enter your game name: ") + ".json"
+    path = Path(__file__).parent / "old_games" / game_name
+
+    with open(path, "r", encoding="utf-8") as f:
+        try:
+            players = json.load(f)
+        except json.JSONDecodeError:
+            players = {}
+
+    if len(players) < 4:
+        print(f"You have fewer than 4 players. {4 - len(players)} more people must register first.")
+
+        while len(players) < 4:
+            while True:
+                if signup():
+                    break
+        
+        print("Now you can log in to your account to continue playing.")
+
     cnt = 0
     while cnt < 4:
         while True:
